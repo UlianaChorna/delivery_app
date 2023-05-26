47212afec313258f14dd883a5f8c11003bc40614
UlianaChorna <110450537+UlianaChorna@users.noreply.github.com>
2023-05-26 21:26:49 +0300

    refactoring

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ console.log("DB Connection")
 
 }).catch((err) => console.log(err))
 
-app.listen(8800, ()=> {
+app.listen(process.env.PORT || 8800, ()=> {
     console.log("Backend server is running")
 })
 
